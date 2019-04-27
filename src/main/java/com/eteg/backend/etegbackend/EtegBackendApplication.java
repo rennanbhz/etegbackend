a7b39c742d73899294f062fc88426eac7de7dfd8
Renan Mattos <rennanbhz@hotmail.com>
2019-04-27 18:53:31 -0300

    Started project with all dependencies

---
+++ src/main/java/com/eteg/backend/etegbackend/EtegBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eteg.backend.etegbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtegBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtegBackendApplication.class, args);
+	}
+
+}
